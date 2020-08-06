06b22a5d2d0b65778b7235ef9407994a3a9c7669
Marius Seufzer <marius.seufzer@code.berlin>
2020-08-06 15:21:34 +0200

    Adjust publishing steps

--- Sources/mariusseufzer-website/main.swift
+++ Sources/mariusseufzer-website/main.swift
@@ -2,6 +2,5 @@ try MariusSeufzerWebsite().publish(using: [
     .addMarkdownFiles(),
     .copyResources(),
     .generateHTML(withTheme: .appStore),
-    .generateSiteMap(),
-    .deploy(using: .gitHub("marius-se/website", useSSH: true))
+    .generateSiteMap()
 ])
